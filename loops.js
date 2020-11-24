ab6dc997fda77859df36ec276efeb86a109d07f9
bold-mixer-0681 <renee.pujol@gmail.com>
2020-11-24 03:18:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,3 +19,15 @@ function whileLoop(n){
   }
   return "done"
 }
+let i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num){
+  do{
+    console.log("I run once regardless.");
+  } while{incrementVariable()>= num
+    
+}
