27c2b1bb430ce42da3ea4f13657c9c9b1624caf8
bold-mixer-0681 <renee.pujol@gmail.com>
2020-11-24 02:57:28 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if( i > 1){
+      array.push(`I am ${i} strange loops.`)
+      }
+
+    else{
+      array.push(`I am ${i} strange loop.`)
+      }
+  }
+  return array
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log( n--)
+  }
+
+}
